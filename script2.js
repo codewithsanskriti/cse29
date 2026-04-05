bd353adaf8746282dac00a8d2b7b6b3b43e64f17
codewithsanskriti <guptasanskriti9266@gmail.com>
2026-04-05 10:08:54 +0530

    Add files via upload

---
+++ script2.js
@@ -0,0 +1,30 @@
+//events in js 
+function
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const x=document.getElementById("p1");
+
+
+// x.innerHTML="<b> sanskriti </b>"
+// const x=document.getElementById("jk").getAttribute("class");
+// document.getElementById("d").innerHtML=x;
+
+// function practice(){
+//     document.open();
+//     document.write
+// }
+
