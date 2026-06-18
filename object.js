a59b3fab092a889e07f1ad019ceb98900945d217
codewithsanskriti <guptasanskriti9266@gmail.com>
2026-06-18 20:54:11 +0530

    Add files via upload

---
+++ object.js
@@ -0,0 +1,14 @@
+<script type="text/javascript">
+        var person={
+            name: John,Mac,
+            age:30,
+            ismarried:true,
+            address{
+                street:'vinay nagar',
+                flatno:201
+            }
+        };
+        console.log(person);
+
+        
+    </script>
